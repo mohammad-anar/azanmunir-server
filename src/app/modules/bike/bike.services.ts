e3aeb22e4221179674b9167d18840892ceb63a34
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-03-07 23:37:43 +0600

    bike serivce completed

--- src/app/modules/bike/bike.services.ts
+++ src/app/modules/bike/bike.services.ts
@@ -1,7 +1,30 @@
 import { Prisma } from "@prisma/client";
+import ApiError from "src/errors/ApiError.js";
 import { prisma } from "src/helpers.ts/prisma.js";
 
 const createBike = async (payload: Prisma.BikeCreateInput) => {
   const result = await prisma.bike.create({ data: payload });
   return result;
 };
+
+// const getAllBikes = async () => {};
+
+const getBikeById = async (id: string) => {
+  const result = await prisma.bike.findUnique({ where: { id } });
+  if (!result) {
+    throw new ApiError(404, "Bike not found!");
+  }
+  return result;
+};
+
+const updateBike = async (id: string, payload: Prisma.BikeUpdateInput) => {
+  const result = await prisma.bike.update({ where: { id }, data: payload });
+  return result;
+};
+
+const deleteBike = async (id: string) => {
+  const result = await prisma.bike.delete({ where: { id } });
+  return result;
+};
+
+export const BikeService = { createBike, getBikeById, updateBike, deleteBike };
