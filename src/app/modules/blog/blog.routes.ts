150752170cf0ee136b30133440b9e394b06675fb
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-03-08 22:55:18 +0600

    blog route completed

---
+++ src/app/modules/blog/blog.routes.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { BlogController } from "./blog.controller.js";
+import auth from "src/app/middlewares/auth.js";
+import { Role } from "@prisma/client";
+
+const router = express.Router();
+
+router.get("/", BlogController.getAllBlogs);
+router.post("/", auth(Role.ADMIN), BlogController.createBlog);
+router.get("/:id", BlogController.getBlogById);
+router.patch("/:id", auth(Role.ADMIN), BlogController.updateBlog);
+router.delete("/:id", auth(Role.ADMIN), BlogController.deleteBlog);
+
+export const BlogRouter = router;
